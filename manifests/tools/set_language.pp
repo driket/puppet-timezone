55c261d1bfecd7187d5289d8a32771a4d54fc810
Cedric Villa <cedric.villa@gmail.com>
2013-07-01 15:33:25 +0200

    code cleanup

--- manifests/tools/set_language.pp
+++ manifests/tools/set_language.pp
@@ -3,15 +3,15 @@ define mv_timezone::tools::set_timezone($language_config = $mv_timezone::params:
 	require('mv_timezone')
 	require('mv_timezone::packages')
 	
-#	file { "${language_config}":
-#		ensure => file,
-#		content => "$timezone\n",
- #       mode 	=> "0644",
-  #      owner 	=> 'root',
-   #     group 	=> 'root',
-		#require => Package['mv_timezone'],
-		#notify => Exec['mv_timezone_setup'],
-		#}
+	#file { "${language_config}":
+	#		ensure 	=> file,
+	#		content => "$timezone\n",
+ 	#       mode 		=> "0644",
+  #       owner 	=> 'root',
+  #       group 	=> 'root',
+	#require 	=> Package['mv_timezone'],
+	#notify 	=> Exec['mv_timezone_setup'],
+	#}
 	
 	#exec { "mv_lanaguage_setup":
 	#	command => "/usr/sbin/dpkg-reconfigure -f noninteractive locales",
